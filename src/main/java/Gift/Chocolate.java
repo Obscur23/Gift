46f6cbbdf433736997b5a8e681ca14f067339160
Obscur23 <obscuredo@mail.ru>
2017-12-14 00:17:21 +0300

    наследование, сетеры, гетеры V 0.5

---
+++ src/main/java/Gift/Chocolate.java
@@ -0,0 +1,14 @@
+package Gift;
+
+/**
+ * Created by Obscur on 13.12.17.
+ */
+class Chocolate extends Sweetness {
+    public Chocolate (String name, float weight, float price) {
+        super(name, weight, price);//наследуем параметры из класса сладость
+    }
+    public String properties (){
+        return "Шоколад"+getName()+"вес"+getWeight()+getPrice();
+        //вытаскиваем параметры через гетер, тк переменные приватные
+    }
+}
--- src/main/java/Gift/Sweetness.java
+++ src/main/java/Gift/Sweetness.java
@@ -1,25 +1,40 @@
 package Gift;
-import java.io.*;
+
 /**
  * Created by Obscur on 10.12.17.
  * Конструктор сладости
  */
-public class Sweetness { //класс сладость для подарка
-    String name; //название
-    float weight; //вес
-    float price; //цена
-    String manuf; //производитель
+public class Sweetness { //класс сладость для подарка, переменные недоступны
+    private String name; //название
+    private float weight; //вес
+    private float price; //цена
     //Конструктор класса
-    public Sweetness(String name, float weight, float price, String manuf){
+    Sweetness(String name, float weight, float price){
     this.name = name;
     this.weight = weight;
     this.price = price;
-    this.manuf = manuf;
     }
-    public void printContent() { //метод для вывода свойств сладости
+    public String getName()  {//можем отдавать название из класса
+        return name;
+    }
+    public void setName(String name){//можем получать название в класс
+        this.name = name;
+    }
+    public float getWeight()  {//можем отдавать вес из класса
+        return weight;
+    }
+    public void setWeight(float weight){//можем получать вес в класс
+        this.weight = weight;
+    }
+    public float getPrice(){
+        return price;
+    }
+    public void setPrice (float price){
+        this.price = price;
+    }
+    public void printSweet() { //метод для вывода свойств сладости
         System.out.println("Name:" + name);
         System.out.println("Weight:" + weight);
         System.out.println("Price:" + price);
-        System.out.println("Manufacturer:" + manuf);
     }
 }
